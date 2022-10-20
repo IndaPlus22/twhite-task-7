040a3e15a9c4ddd5275a7d7605e02b58603e3673
Tim White <tfy21twe@cs.umu.se>
2022-10-20 20:36:29 +0200

    forgot to delete print tests lol

--- cyber-clara.c
+++ cyber-clara.c
@@ -22,26 +22,21 @@ int main() {
         strcat(names[i], surNames[i]);
     }
 
-
-
     char duplicates[n][20];
     bool previousDuplicateFound = false;
     int counter = 0;
 
     for (int i = 0; i < n; i++) {
         previousDuplicateFound = false;
-        printf("iteration %d\n", i);
         //Look for duplicates already found
         for (int j = 0; j < n; j++) {
             if (strcmp(duplicates[j], names[i]) == 0) {
                 previousDuplicateFound = true;
-                printf("duplicate found: %s\n", duplicates[j]);
                 break;
             }
         }
         //If found move to next iteration
         if (previousDuplicateFound == true) {
-            printf("continue\n");
             continue;
         }
 
@@ -49,15 +44,9 @@ int main() {
         for (int k = i+1; k < n; k++) {
                 if (strcmp(names[i], names[k]) == 0) {
                     strcpy(duplicates[i], names[k]);
-                    counter++;
-                    printf("duplicate nr %d happened: %s\n", counter, duplicates[counter]);
+                    counter++;                
                 }
         }
-    }
-    
-
-    for (int i = 0; i < counter; i++) {
-        printf("duplicates: %s \n", duplicates[i]);
     }
 
     printf("%d\n", n-counter);
